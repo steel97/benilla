5edd460e46eeaca32c4cb3915dcb8165322b4c35
Ivan Yv <ivan.yurkov@steel-team.net>
2026-08-05 10:59:36 +0300

    chore: refactoring

--- mobile/src/lib.rs
+++ mobile/src/lib.rs
@@ -1,86 +1,26 @@
 //! A 3d Scene with a button and playing sound.
 
-use benilla::main_shared;
 use bevy::prelude::bevy_main;
-use std::ffi::CStr;
-use std::path::PathBuf;
 
-pub fn transform_path_ios(relative_path: &str) -> PathBuf {
-    if let Ok(mut exe_path) = std::env::current_exe() {
-        if exe_path.pop() {
-            return exe_path.join(relative_path);
-        }
-    }
-    PathBuf::from("").join(relative_path)
-}
+use crate::{
+    helpers::{postprocess_env, transform_path},
+    runner::run_benilla,
+};
 
-#[cfg(target_os = "ios")]
-#[unsafe(no_mangle)]
-pub extern "C" fn get_dim_ios() -> *mut std::os::raw::c_char {
-    use objc2_foundation::MainThreadMarker;
-    use objc2_ui_kit::UIScreen;
-    use std::ffi::CString;
-
-    unsafe {
-        if let Some(mtm) = MainThreadMarker::new() {
-            let main_screen = UIScreen::mainScreen(mtm);
-
-            /*let size_str = if let Some(current_mode) = main_screen.currentMode() {
-                let size = current_mode.size();
-                format!("{}x{}", size.height as u32, size.width as u32)
-            } else {*/
-            let bounds = main_screen.bounds();
-            let scale = main_screen.scale();
-            let size_str = format!(
-                "{}x{}",
-                (bounds.size.height) as u32,
-                (bounds.size.width) as u32
-            );
-            //};
-
-            let c_str = CString::new(size_str).unwrap_or_else(|_| CString::new("0x0").unwrap());
-            c_str.into_raw()
-        } else {
-            panic!("failed to take screen resolution");
-        }
-    }
-}
-
-#[cfg(target_os = "ios")]
-#[unsafe(no_mangle)]
-pub extern "C" fn free_dim_ios(ptr: *mut std::os::raw::c_char) {
-    if !ptr.is_null() {
-        unsafe {
-            let _ = std::ffi::CString::from_raw(ptr);
-        }
-    }
-}
+pub mod helpers;
+pub mod runner;
 
 #[bevy_main]
 pub fn main() {
     unsafe {
-        #[cfg(target_os = "ios")]
-        {
-            unsafe {
-                let ptr = get_dim_ios();
-
-                if !ptr.is_null() {
-                    let c_str = CStr::from_ptr(ptr);
-                    let size_string = c_str.to_string_lossy().into_owned();
-                    println!("size {}", size_string);
-                    std::env::set_var("WOW_WIN", size_string);
-                }
-
-                free_dim_ios(ptr);
-            }
-        }
-
         std::env::set_var("WOW_HOST", "192.168.1.20:3724");
         std::env::set_var("WOW_USER", "ivan");
         std::env::set_var("WOW_PASS", "changeme");
 
-        std::env::set_var("WOW_DATA", transform_path_ios("WoW/Data").to_str().unwrap());
-        std::env::set_var("WOW_BUILTIN", transform_path_ios("").to_str().unwrap());
+        std::env::set_var("WOW_DATA", transform_path("WoW/Data").to_str().unwrap());
+        std::env::set_var("WOW_BUILTIN", transform_path("").to_str().unwrap());
     }
-    main_shared();
+
+    postprocess_env();
+    run_benilla();
 }
---
+++ mobile/src/runner.rs
@@ -0,0 +1,3 @@
+pub fn run_benilla() {
+    benilla::main();
+}
